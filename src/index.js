298295c0295667437ec3baaf8a52fdaf9708949d
imkaranverma <106688614+imkaranverma@users.noreply.github.com>
2024-03-09 23:06:48 +0530

    done 2.0

--- src/index.js
+++ src/index.js
@@ -13,7 +13,7 @@ import { ApolloProvider, ApolloClient, InMemoryCache } from '@apollo/client';
 
 
 const client = new ApolloClient({
-  uri: 'http://localhost:3000/graphql',
+  uri: 'http://localhost:3001/graphql',
   cache: new InMemoryCache()
 });
 
